c2037ab25ade145ddb827c67c105e98e7b6d9ba7
Hrishav Ranjan <rishavranjan2395@gmail.com>
2026-05-01 18:38:37 +0530

    connected render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://team-task-manager-48k6.onrender.com/"
 });
 
 // 🔥 Attach token to every request
